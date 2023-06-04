d74c2504b07ca5c989114fa3d0e21ccb4ff18737
Brandon Geraci <brandon.geraci@gmail.com>
2023-06-04 09:41:00 -0500

    Removing the scripts and checking the c++ code

--- mkapp/app/script/formatsd.sh
+++ mkapp/app/script/formatsd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/mnt/app/script/umountsd.sh
+# /mnt/app/script/umountsd.sh
 mkfs.vfat -F 32 /dev/mmcblk0
-/mnt/app/script/mountsd.sh
+# /mnt/app/script/mountsd.sh
 
--- mkapp/app/services/scripts/dosfstools_startup.sh
+++ mkapp/app/services/scripts/dosfstools_startup.sh
@@ -5,5 +5,5 @@ ln -sfn /mnt/app/services/dosfstools/fsck.fat /bin/fsck.fat
 ln -sfn /mnt/app/services/dosfstools/mkfs.fat /bin/mkfs.fat
 
 # Repair SD Card if needed
-/bin/fsck.fat -a /dev/mmcblk0 > /tmp/fsck.log 2>&1
-mv /tmp/fsck.log /mnt/extsd/
+# /bin/fsck.fat -a /dev/mmcblk0 > /tmp/fsck.log 2>&1
+# mv /tmp/fsck.log /mnt/extsd/
